04d5834aa71a5b825645a68d14cf7cf771f0b7fa
deedeeh <dinahafez.8@gmail.com>
2022-02-08 20:34:51 +0000

    Refactored isFromManchester

--- src/main/scala/com/techreturners/exercise002/Exercise002.scala
+++ src/main/scala/com/techreturners/exercise002/Exercise002.scala
@@ -2,11 +2,10 @@ package com.techreturners.exercise002
 
 class Exercise002 {
   def isFromManchester(person: Person): Boolean = {
-      var result: Boolean = false
       if(person.city ==  "Manchester") {
-        result = true
+        return true
       }
-      result
+      false
     }
 
     def canWatchFilm(/*person: Person, ageLimit: Int */): Boolean = false
